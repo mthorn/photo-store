b90afe259625e0147c0a4f19cfa8bf2f405112ad
Mark Thorn <mcthorn@gmail.com>
2015-08-08 20:44:03 -0700

    Use DB connection pool in background jobs.

--- app/jobs/process_upload_job.rb
+++ app/jobs/process_upload_job.rb
@@ -3,7 +3,9 @@ class ProcessUploadJob < ActiveJob::Base
   queue_as :default
 
   def perform(model, field)
-    model.fetch_and_process(field.to_sym)
+    ActiveRecord::Base.connection_pool.with_connection do
+      model.fetch_and_process(field.to_sym)
+    end
   end
 
 end
--- app/jobs/restart_interrupted_upload_processing_job.rb
+++ app/jobs/restart_interrupted_upload_processing_job.rb
@@ -3,13 +3,15 @@ class RestartInterruptedUploadProcessingJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    count = 0
-    Upload.where(state: 'process').find_each do |upload|
-      count += 1
-      ProcessUploadJob.perform_later(upload, 'file')
-    end
-    if count.nonzero?
-      logger.info "Restarted #{count} upload processing jobs"
+    ActiveRecord::Base.connection_pool.with_connection do
+      count = 0
+      Upload.where(state: 'process').find_each do |upload|
+        count += 1
+        ProcessUploadJob.perform_later(upload, 'file')
+      end
+      if count.nonzero?
+        logger.info "Restarted #{count} upload processing jobs"
+      end
     end
   end
 
