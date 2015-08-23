bc89c6b1010b4e5b4ed6148e9cfc768255a54fcb
Mark Thorn <mcthorn@gmail.com>
2015-08-22 17:21:53 -0700

    Store video/mp4 in S3 for MOV files.

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,7 +1,17 @@
 class VideoUploader < BaseUploader
 
+  MIME_OVERRIDES = {
+    'video/quicktime' => 'video/mp4',
+    'video/x-m4v' => 'video/mp4'
+  }
+
   def extension_white_list
     %w( mp4 m4v mov )
+  end
+
+  def fog_attributes
+    mime = self.model.mime
+    { 'Content-Type' => (MIME_OVERRIDES[mime] || mime) }
   end
 
 end
