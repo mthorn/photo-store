1bb030655170dcef5a620c3c948df527b2de1d90
Mark Thorn <mcthorn@gmail.com>
2015-08-23 20:32:58 -0700

    Configurable worker limit.

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -2,6 +2,8 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sucker_punch
 end
 
+ActiveJob::QueueAdapters::SuckerPunchAdapter::JobWrapper.workers((ENV['WORKER_COUNT'] || 2).to_i)
+
 if defined? Rails::Server
   RestartInterruptedUploadProcessingJob.perform_later
 end
